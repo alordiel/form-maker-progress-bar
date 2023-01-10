dad9c9cafffb21bcbfe04dc34ad4995239b37bdc
alordiel <alexander.vasilev@protonmail.com>
2023-01-10 10:33:44 +0200

    initial php file

---
+++ form-maker-progress-bar.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Progress bar for Form Maker
+Plugin URI: https://github.com/alordiel/form-maker-progress-bar
+Description: With the help of a shortcode you can add a progress bar for your forms, when you use them as petitions.
+Version: 1.0.0
+Author: Alexander Vasilev
+Author URI: https://timelinedev.com
+License: GPLv2
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
